e917ba845e8635abf6c8b80aa009467d409ef068
akosgarai <szlem01@gmail.com>
2021-09-30 19:33:40 +0200

    Add menu hook & also update route

--- magnet_xml_import.php
+++ magnet_xml_import.php
@@ -169,19 +169,21 @@ function magnet_xml_import_civicrm_themes(&$themes)
 //
 //}
 
+// The functions below are implemented by me.
 /**
  * Implements hook_civicrm_navigationMenu().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
  */
-//function magnet_xml_import_civicrm_navigationMenu(&$menu) {
-//  _magnet_xml_import_civix_insert_navigation_menu($menu, 'Mailings', array(
-//    'label' => E::ts('New subliminal message'),
-//    'name' => 'mailing_subliminal_message',
-//    'url' => 'civicrm/mailing/subliminal',
-//    'permission' => 'access CiviMail',
-//    'operator' => 'OR',
-//    'separator' => 0,
-//  ));
-//  _magnet_xml_import_civix_navigationMenu($menu);
-//}
+function magnet_xml_import_civicrm_navigationMenu(&$menu)
+{
+    _magnet_xml_import_civix_insert_navigation_menu($menu, 'Contributions', [
+        'label' => E::ts('Magnet XML Import'),
+        'name' => 'magnet_xml_import',
+        'url' => 'civicrm/contribute/manget-xml',
+        'permission' => 'administer CiviCRM,access CiviContribute,edit contributions',
+        'operator' => 'AND',
+        'separator' => 0,
+    ]);
+    _magnet_xml_import_civix_navigationMenu($menu);
+}
