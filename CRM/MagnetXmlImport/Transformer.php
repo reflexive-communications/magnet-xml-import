0f1da4bc55df4a296b2dc88b77d0ea87a7de1ab5
akosgarai <szlem01@gmail.com>
2021-10-01 15:33:07 +0200

    Transform transaction data to contribution data

--- CRM/MagnetXmlImport/Transformer.php
+++ CRM/MagnetXmlImport/Transformer.php
@@ -7,6 +7,7 @@ use CRM_MagnetXmlImport_ExtensionUtil as E;
  */
 class CRM_MagnetXmlImport_Transformer
 {
+    const CRM_COMPLETED_STATUS_ID = 1;
     /**
      * Transform Magnet transaction data to civicrm contact data
      *
@@ -21,6 +22,29 @@ class CRM_MagnetXmlImport_Transformer
             'contact_type' => 'Individual',
             'display_name' => (string) $transaction->Ellenpartner,
             $bankAccountNumberContactParamName => (string) $transaction->Ellenszamla,
+        ];
+    }
+
+    /**
+     * Transform Magnet transaction data to civicrm contribution data
+     *
+     * @param SimpleXMLElement $transaction Magnet transaction object
+     * @param array $config the configuration for the contribution mapping
+     *
+     * @return array contributionData
+     */
+    public static function magnetTransactionToContribution(SimpleXMLElement $transaction, array $config): array
+    {
+        return [
+            'trxn_id' => (string) $transaction->Tranzakcioszam,
+            'total_amount' => (float) $transaction->Osszeg,
+            'currency' => (string) $transaction->Osszeg->attributes()['Devizanem'],
+            'message' => (string) $transaction->Kozlemeny,
+            'receive_date' => str_replace('.', '', (string) $transaction->Esedekessegnap), // we need 20150131
+            'source' => $config['source'],
+            'financial_type_id' => $config['financialTypeId'],
+            'payment_instrument_id' => $config['paymentInstrumentId'],
+            'contribution_status_id' => self::CRM_COMPLETED_STATUS_ID,
         ];
     }
 }
