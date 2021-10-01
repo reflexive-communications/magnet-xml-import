632a23309f977ce06e37961fc52cd5ea1415c431
akosgarai <szlem01@gmail.com>
2021-10-01 21:02:01 +0200

    Run php-cs-fixer tool on the codebase

--- tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
+++ tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
@@ -3,30 +3,33 @@
 /**
  * This is a generic test class for the extension (implemented with PHPUnit).
  */
-class CRM_MagnetXmlImport_TransformerTest extends \PHPUnit\Framework\TestCase {
+class CRM_MagnetXmlImport_TransformerTest extends \PHPUnit\Framework\TestCase
+{
 
   /**
    * The setup() method is executed before the test is executed (optional).
    */
-  public function setUp() {
-    parent::setUp();
-  }
+    public function setUp()
+    {
+        parent::setUp();
+    }
 
-  /**
-   * The tearDown() method is executed after the test was executed (optional)
-   * This can be used for cleanup.
-   */
-  public function tearDown() {
-    parent::tearDown();
-  }
+    /**
+     * The tearDown() method is executed after the test was executed (optional)
+     * This can be used for cleanup.
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
 
-  /**
-   * Simple example test case.
-   *
-   * Note how the function name begins with the word "test".
-   */
-  public function testExample() {
-    self::assertTrue(TRUE, "The argument must be true to pass the test");
-  }
-
+    /**
+     * Simple example test case.
+     *
+     * Note how the function name begins with the word "test".
+     */
+    public function testExample()
+    {
+        self::assertTrue(true, "The argument must be true to pass the test");
+    }
 }
