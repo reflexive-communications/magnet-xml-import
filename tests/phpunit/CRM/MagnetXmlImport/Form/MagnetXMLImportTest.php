9db269b168bb63766c7ce516e351cbce559bc05f
akosgarai <szlem01@gmail.com>
2021-10-02 17:06:36 +0200

    Testcases for the admin form

--- tests/phpunit/CRM/MagnetXmlImport/Form/MagnetXMLImportTest.php
+++ tests/phpunit/CRM/MagnetXmlImport/Form/MagnetXMLImportTest.php
@@ -6,16 +6,7 @@ use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * Testcases for the Admin form functionalities.
  *
  * @group headless
  */
@@ -23,37 +14,128 @@ class CRM_MagnetXmlImport_Form_MagnetXMLImportTest extends \PHPUnit\Framework\Te
 {
     public function setUpHeadless()
     {
-        // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-        // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
         return \Civi\Test::headless()
       ->installMe(__DIR__)
       ->apply();
     }
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
     }
 
     /**
-     * Example: Test that a version is returned.
+     * PreProcess test case pre process does nothing.
      */
-    public function testWellFormedVersion()
+    public function testPreProcess()
     {
-        $this->assertNotEmpty(E::SHORT_NAME);
-        $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+        $form = new CRM_MagnetXmlImport_Form_MagnetXMLImport();
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
     }
 
     /**
-     * Example: Test that we're using a fake CMS.
+     * setDefaultValues test case.
      */
-    public function testWellFormedUF()
+    public function testSetDefaultValues()
     {
-        $this->assertEquals('UnitTests', CIVICRM_UF);
+        $form = new CRM_MagnetXmlImport_Form_MagnetXMLImport();
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
+        $expectedConfig = [
+            'source' => 'Magnet Bank',
+            'financialTypeId' => 1,
+            'paymentInstrumentId' => 5,
+            'bankAccountNumberParameter' => 'custom_1',
+            'onlyIncome' => 1,
+        ];
+        self::assertSame($expectedConfig, $form->setDefaultValues());
+    }
+
+    /**
+     * Build quick form test case.
+     * One custom contact param is created
+     * to be able to test that branch.
+     */
+    public function testBuildQuickForm()
+    {
+        $customGroup = \Civi\Api4\CustomGroup::create(false)
+            ->addValue('title', 'TestCustomGroup')
+            ->addValue('extends', 'Contact')
+            ->addValue('is_active', true)
+            ->execute()
+            ->first();
+        \Civi\Api4\CustomField::create()
+            ->addValue('custom_group_id', $customGroup['id'])
+            ->addValue('label', 'bank account number')
+            ->addValue('data_type', 'String')
+            ->addValue('html_type', 'Text')
+            ->execute();
+        $form = new CRM_MagnetXmlImport_Form_MagnetXMLImport();
+        $form->setVar('_gid', null);
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
+        self::assertEmpty($form->buildQuickForm());
+    }
+
+    /**
+     * addRules test case.
+     */
+    public function testAddRules()
+    {
+        $form = new CRM_MagnetXmlImport_Form_MagnetXMLImport();
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
+        self::assertEmpty($form->addRules());
+    }
+
+    /**
+     * fileExtension test cases.
+     */
+    public function testFileExtensionValidFile()
+    {
+        $fileData = [
+            'importSource' => [
+                'type' => 'text/xml',
+            ],
+        ];
+        $result = CRM_MagnetXmlImport_Form_MagnetXMLImport::fileExtension([], $fileData);
+        self::assertTrue($result);
+    }
+    public function testFileExtensionNotValidFile()
+    {
+        $fileData = [
+            'importSource' => [
+                'type' => 'text/notxml',
+            ],
+        ];
+        $result = CRM_MagnetXmlImport_Form_MagnetXMLImport::fileExtension([], $fileData);
+        self::assertTrue(array_key_exists('importSource', $result));
+    }
+
+    /**
+     * PostProcess test case.
+     */
+    public function testPostProcess()
+    {
+        $submitValues = [
+            'source' => 'Magnet Bank',
+            'financialTypeId' => 1,
+            'paymentInstrumentId' => 5,
+            'bankAccountNumberParameter' => 'custom_1',
+            'onlyIncome' => 1,
+        ];
+        $submitFiles = [
+            'importSource' => [
+                'type' => 'text/notxml',
+                'tmp_name' => __DIR__.'/sampleData.xml',
+            ],
+        ];
+        $form = new CRM_MagnetXmlImport_Form_MagnetXMLImport();
+        $form->setVar('_submitValues', $submitValues);
+        $form->setVar('_submitFiles', $submitFiles);
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
+        self::assertEmpty($form->postProcess(), 'PostProcess supposed to be empty.');
     }
 }
