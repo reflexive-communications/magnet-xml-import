ea8a8779d3d50abffeb87d651b51f352028cebf7
akosgarai <szlem01@gmail.com>
2021-10-02 15:11:28 +0200

    Implement transformer tests

--- tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
+++ tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
@@ -9,7 +9,7 @@ class CRM_MagnetXmlImport_TransformerTest extends \PHPUnit\Framework\TestCase
   /**
    * The setup() method is executed before the test is executed (optional).
    */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
     }
@@ -18,18 +18,56 @@ class CRM_MagnetXmlImport_TransformerTest extends \PHPUnit\Framework\TestCase
      * The tearDown() method is executed after the test was executed (optional)
      * This can be used for cleanup.
      */
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
     }
 
     /**
-     * Simple example test case.
-     *
-     * Note how the function name begins with the word "test".
+     * magnetTransactionToContact test case.
      */
-    public function testExample()
+    public function testMagnetTransactionToContact()
     {
-        self::assertTrue(true, "The argument must be true to pass the test");
+        $contactDisplayName = 'Teszt Contact01';
+        $bankAccountNumber = 'HU20 1111 2222 3333 4445 0000 0000';
+        $xmlString = '<Tranzakcio><Tranzakcioszam>trxn003</Tranzakcioszam><Osszeg Devizanem="HUF">1000.00</Osszeg><Esedekessegnap>2016.01.15.</Esedekessegnap><Ellenpartner>'.$contactDisplayName.'</Ellenpartner><Ellenszamla>'.$bankAccountNumber.'</Ellenszamla></Tranzakcio>';
+        $bankAccountName = 'custom_1';
+        $xml = simplexml_load_string($xmlString);
+        $transformed = CRM_MagnetXmlImport_Transformer::magnetTransactionToContact($xml, $bankAccountName);
+        $expected = [
+            'contact_type' => 'Individual',
+            'display_name' => $contactDisplayName,
+            $bankAccountName => $bankAccountNumber,
+        ];
+        self::assertSame($expected, $transformed);
+    }
+
+    /**
+     * magnetTransactionToContribution test case.
+     */
+    public function testMagnetTransactionToContribution()
+    {
+        $config = [
+            'source' => 'test source',
+            'financialTypeId' => '1',
+            'payment_instrument_id' => '1',
+        ];
+        $trxnId = 'trxn003';
+        $amount = '1000.00';
+        $currency = 'HUF';
+        $xmlString = '<Tranzakcio><Tranzakcioszam>'.$trxnId.'</Tranzakcioszam><Osszeg Devizanem="'.$currency.'">'.$amount.'</Osszeg><Esedekessegnap>2016.01.15.</Esedekessegnap><Ellenpartner>Teszt Contact01</Ellenpartner><Ellenszamla>HU20 1111 2222 3333 4445 0000 0000</Ellenszamla></Tranzakcio>';
+        $xml = simplexml_load_string($xmlString);
+        $transformed = CRM_MagnetXmlImport_Transformer::magnetTransactionToContribution($xml, $config);
+        $expected = [
+            'trxn_id' => $trxnId,
+            'total_amount' => (float) $amount,
+            'currency' => $currency,
+            'receive_date' => '20160115',
+            'source' => $config['source'],
+            'financial_type_id' => $config['financialTypeId'],
+            'payment_instrument_id' => $config['payment_instrument_id'],
+            'contribution_status_id' => CRM_MagnetXmlImport_Transformer::CRM_COMPLETED_STATUS_ID,
+        ];
+        self::assertSame($expected, $transformed);
     }
 }
