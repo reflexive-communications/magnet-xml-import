fd937144dd08e49720de81913937ea5757728603
Sandor Semsey <sandor@es-progress.hu>
2022-09-13 15:42:09 +0200

    format with php-cs-fixer

--- tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
+++ tests/phpunit/CRM/MagnetXmlImport/TransformerTest.php
@@ -5,7 +5,6 @@
  */
 class CRM_MagnetXmlImport_TransformerTest extends \PHPUnit\Framework\TestCase
 {
-
     /**
      * The setup() method is executed before the test is executed (optional).
      */
